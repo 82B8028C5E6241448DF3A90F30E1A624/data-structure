05bf5afdbddbdf2b9667069aca8ad8226b26a6eb
jie-smile <bestjie21@outlook.com>
2020-03-23 11:19:27 +0800

    Linked List v1.0
    Resolve #5
    
    Signed-off-by: jie-smile <bestjie21@outlook.com>

--- LinkList.h
+++ LinkList.h
@@ -57,4 +57,113 @@ class LinkList {
 
   iterator begin() const { return iterator(head); }
   iterator end() const { return iterator(nullptr); }
+
+  bool isEmpty() { return size == 0; }
+
+  T get(int index) {
+    if (index >= size || index < 0) exit(1);
+
+    ListNode *node = head;
+    for (int i = 1; i <= index; i++) {
+      node = node->next;
+    }
+    return node->data;
+  }
+
+  bool set(int index, T _data) {
+    if (index >= size || index < 0) return false;
+    ListNode *node = head;
+    for (int i = 1; i <= index; i++) {
+      node = node->next;
+    }
+    node->data = _data;
+    return true;
+  }
+
+  void insert(int index, T _data) {
+    if (index > size || index < 0) exit(0);
+
+    ListNode *temp = head;
+    ListNode *prev = nullptr;
+    ListNode *node = new ListNode();
+    node->data = _data;
+
+    if (index == size) {
+      temp = nullptr;
+      prev = tail;
+      tail = node;
+    } else {
+      int n = 0;
+      while (n < index) {
+        prev = temp;
+        temp = temp->next;
+        n++;
+      }
+    }
+
+    if (temp == head) {
+      node->next = head;
+      head = node;
+    } else {
+      node->next = temp;
+      prev->next = node;
+    }
+    size++;
+  }
+
+  T remove(int index) {
+    if (index >= size || index < 0) exit(0);
+
+    int n = 0;
+    ListNode *temp = head;
+    ListNode *prev = nullptr;
+    while (n < index) {
+      prev = temp;
+      temp = temp->next;
+      n++;
+    }
+
+    T value = temp->data;
+
+    if (temp == head) {
+      head = head->next;
+    } else if (temp = tail) {
+      prev->next = nullptr;
+      tail = prev;
+    } else {
+      prev->next = temp->next;
+    }
+
+    temp->next = nullptr;
+    delete (temp);
+    size--;
+    return value;
+  }
+
+  int removeAll(T _data) {
+    int count = 0;
+    int index = 0;
+    ListNode *node = head;
+    while (node) {
+      ListNode *temp = node;
+      node = node->next;
+      if (temp->data == _data) {
+        remove(index);
+        count++;
+      } else {
+        index++;
+      }
+    }
+    return count;
+  }
+
+  int contains(T _data) {
+    int count = 0;
+    ListNode *node = head;
+    for (int index = 0; index < size; index++) {
+      if (node->data == _data) count++;
+      node = node->next;
+    }
+    return count;
+  }
 };
--- main.cc
+++ main.cc
@@ -1,20 +1,29 @@
-#include "gtest/gtest.h"
+#include <iostream>
 #include "LinkList.h"
+#include "gtest/gtest.h"
 
 TEST(Suite1, Test1) {
   LinkList<int> list;
+  EXPECT_EQ(list.isEmpty(), true);
+
   const int N = 5;
-
   for (int i = 0; i < N; i++) {
-    list.insert(i + 1);
+    list.insert(1);
   }
 
-  int n = N;
-  for (int d : list) {
-    EXPECT_EQ(d, n);
-    n--;
-  }
-  EXPECT_EQ(n, 0);
+  EXPECT_EQ(list.isEmpty(), false);
+
+  EXPECT_EQ(list.get(0), 1);
+
+  EXPECT_EQ(list.contains(1), 5);
+
+  list.set(4, 9);
+  EXPECT_EQ(list.get(4), 9);
+
+  int value = list.remove(0);
+  EXPECT_EQ(value, 1);
+  
+  EXPECT_EQ(list.removeAll(1), 3);
 }
 
 int main(int argc, char **argv) {
